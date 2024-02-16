9dabb91493257cc23b7499ee3f0895d29b07a7f9
siwon99 <kimsiwon0707@gmail.com>
2024-02-16 16:07:20 +0900

    Docs: Navbar 오류 수정

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, animateScroll as scroll } from 'react-scroll';
 import NavbarBackground from './NavbarBackground'
 import '../main.css'
@@ -7,17 +7,24 @@ interface NavbarProps {
   to: string;
   label: string;
   className: string;
+  onClick?: () => void; // onClick prop 추가
 }
 
-const NavbarItem: React.FC<NavbarProps> = ({ to, label, className }) => (
+const NavbarItem: React.FC<NavbarProps> = ({ to, label, className, onClick }) => (
   <li>
-    <Link to={to} smooth={true} duration={500} className={className}>
+    <Link to={to} smooth={true} duration={500} className={className} onClick={onClick}>
       {label}
     </Link>
   </li>
 );
 
 const Navbar: React.FC = () => {
+  const [activeItem, setActiveItem] = useState("home"); // 초기 선택된 항목은 "home"입니다.
+
+  const handleItemClick = (itemName: string) => {
+    setActiveItem(itemName); // 클릭된 항목을 상태에 반영합니다.
+  };
+
   const scrollToTop = () => {
     scroll.scrollToTop();
   };
@@ -31,11 +38,11 @@ const Navbar: React.FC = () => {
           </Link>
         </div>
         <ul className="navbar__menu">
-          <NavbarItem to="home" label="Home" className="navbar__menu__item" />
-          <NavbarItem to="about" label="About" className="navbar__menu__item" />
-          <NavbarItem to="skills" label="Skills" className="navbar__menu__item" />
-          <NavbarItem to="work" label="My Projects" className="navbar__menu__item" />
-          <NavbarItem to="contact" label="Contact" className="navbar__menu__item" />
+          <NavbarItem to="home" label="Home" className={activeItem === "home" ? "navbar__menu__item active" : "navbar__menu__item"} onClick={() => handleItemClick("home")} />
+          <NavbarItem to="about" label="About" className={activeItem === "about" ? "navbar__menu__item active" : "navbar__menu__item"} onClick={() => handleItemClick("about")} />
+          <NavbarItem to="skills" label="Skills" className={activeItem === "skills" ? "navbar__menu__item active" : "navbar__menu__item"} onClick={() => handleItemClick("skills")} />
+          <NavbarItem to="work" label="My Projects" className={activeItem === "work" ? "navbar__menu__item active" : "navbar__menu__item"} onClick={() => handleItemClick("work")} />
+          <NavbarItem to="contact" label="Contact" className={activeItem === "contact" ? "navbar__menu__item active" : "navbar__menu__item"} onClick={() => handleItemClick("contact")} />
         </ul>
 
         {/* Toggle button */}
